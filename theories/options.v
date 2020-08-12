c76d4beae2faa85f02c0adcbb38cc501e615c261
Ralf Jung <jung@mpi-sws.org>
2020-08-12 18:44:18 +0200

    add options file for library-wide configuration

---
+++ theories/options.v
@@ -0,0 +1,5 @@
+(** Coq configuration for Iris (not meant to leak to clients) *)
+(* Everything here should be [Export Set], which means when this
+file is *imported*, the option will only apply on the import site
+but not transitively. *)
+Export Set Default Proof Using "Type".
