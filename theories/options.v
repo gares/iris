fac5fb7a4ebfa46816faa2f3ebdfef4443b9fa75
Ralf Jung <jung@mpi-sws.org>
2020-09-11 11:16:45 +0200

    improve options-import check

--- theories/options.v
+++ theories/options.v
@@ -5,3 +5,8 @@ but not transitively. *)
 
 Export Set Default Proof Using "Type".
 Export Set Suggest Proof Using. (* also warns about forgotten [Proof.] *)
+
+(* "Fake" import to whitelist this file for the check that ensures we import
+this file everywhere.
+From iris Require Import options.
+*)
