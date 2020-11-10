36e790f8eede301233fd4f47564c3ec62f0857df
Ralf Jung <jung@mpi-sws.org>
2020-11-10 18:31:28 +0100

    fix build

--- theories/heap_lang/total_adequacy.v
+++ theories/heap_lang/total_adequacy.v
@@ -9,7 +9,7 @@ Definition heap_total Σ `{!heapPreG Σ} s e σ φ :
   sn erased_step ([e], σ).
 Proof.
   intros Hwp; eapply (twp_total _ _); iIntros (?) "".
-  iMod (gen_heap_init σ.(heap)) as (?) "Hh".
+  iMod (gen_heap_init σ.(heap)) as (?) "[Hh _]".
   iMod (inv_heap_init loc (option val)) as (?) ">Hi".
   iMod (proph_map_init [] σ.(used_proph_id)) as (?) "Hp".
   iModIntro.
